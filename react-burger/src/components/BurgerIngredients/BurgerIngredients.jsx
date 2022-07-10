8e6e8aea58a7f75fae669eefbdd3cb4c38f26005
PavelBiriukov <90255443+PavelBiriukov@users.noreply.github.com>
2022-07-10 14:28:12 +0600

    com-1/2

--- react-burger/src/components/BurgerIngredients/BurgerIngredients.jsx
+++ react-burger/src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import {Tab} from '@ya.praktikum/react-developer-burger-ui-components';
+import {
+  Tab
+} from '@ya.praktikum/react-developer-burger-ui-components';
 import stylesIngredients from './BurgerIngredients.module.css';
 import CardIngredients from '../CardIngredients/CardIngredients';
 import PropTypes from 'prop-types';
 
+
 const BurgerIngredients = (props) => {
   const [current, setCurrent] = React.useState('one');
+
   return (
     <section className={stylesIngredients.section}>
       <h1 className={`text text_type_main-large mt-10 mb-5`}>Соберите бургер</h1>
       <nav>
         <ul className={stylesIngredients.list}>
           <li>
+
             <Tab value="one" active={current === 'one'} onClick={setCurrent}>
               Булки
             </Tab>
@@ -31,25 +36,25 @@ const BurgerIngredients = (props) => {
       <div className={stylesIngredients.cards}>
         <h2 className={`mt-10 mb-6 text text_type_main-medium`}>Булки</h2>
         <div className={`${`${stylesIngredients.cardsItem} pl-4 pr-2`} pl-4 pr-2`}>
-          {props.data.map((card) => {
+          {props.state.data.map((card) => {
             if (card.type === 'bun') {
-              return <CardIngredients props={card} key={card._id} />
+              return <CardIngredients card={card} key={card._id} active={props.active} setActive={props.setActive} setData={props.setCard} />
             }
           })}
         </div>
         <h2 className={`mt-10 mb-6 text text_type_main-medium`}>Соусы</h2>
-        <div className={`${stylesIngredients.cardsItem} pl-4 pr-2`}>
-          {props.data.map((card) => {
+        <div className={`${stylesIngredients.cardsItem} pl-4 pr-2`} >
+          {props.state.data.map((card) => {
             if (card.type === 'sauce') {
-              return <CardIngredients props={card} key={card._id} />
+              return <CardIngredients card={card} key={card._id} active={props.active} setActive={props.setActive} setData={props.setCard} />
             }
           })}
         </div>
         <h2 className={`mt-10 mb-6 text text_type_main-medium`}>Начинки</h2>
         <div className={`${stylesIngredients.cardsItem} pl-4 pr-2`}>
-          {props.data.map((card) => {
+          {props.state.data.map((card) => {
             if (card.type === 'main') {
-              return <CardIngredients props={card} key={card._id} />
+              return <CardIngredients card={card} key={card._id} active={props.active} setActive={props.setActive} setData={props.setCard} />
             }
           })}
         </div>
@@ -58,7 +63,11 @@ const BurgerIngredients = (props) => {
   )
 }
 
-BurgerIngredients.propTypes ={
-  data: PropTypes.array.isRequired
+BurgerIngredients.propTypes = {
+    state: PropTypes.object,
+    active: PropTypes.bool,
+    setActive: PropTypes.func,
+    setData: PropTypes.func,
+    key: PropTypes.string
 }
 export default BurgerIngredients;
